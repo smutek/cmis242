1802b795672e6a33c97cbecfc681937617b44798
Jimmy Smutek <jimmy@smutek.net>
2018-11-26 21:33:11 -0500

    Week 6 discussion

---
+++ week_5/src/discussion/W5D.java
@@ -0,0 +1,26 @@
+package discussion;
+/*
+ * File: W5D.java
+ * Author: Jimmy Smutek
+ * Date: November 25, 2018
+ * Purpose: Week Five Discussion
+ */
+
+import java.text.NumberFormat;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class W5D extends JFrame {
+
+  //  Car
+  // Color
+  // Red, Blue, Silver :: Base
+  // Options
+  // 4WD, Turbo, Rims, Radar, Lasers
+  private ButtonGroup checkGroup, radioGRoup;
+  private String[] manufacturers = {"Bugati", "Dodge", "Pacer"};
+
+
+
+}
---
+++ week_6/src/discussion/EuclideanRecursion.java
@@ -0,0 +1,61 @@
+package discussion;
+/*
+ * File: EuclideanRecursion.java
+ * Author: Jimmy Smutek
+ * Date: November 26, 2018
+ * Purpose: Week 6 discussion
+ */
+
+public class EuclideanRecursion {
+
+  /*
+    The Euclidean algorithm is a method for computing the greatest common divisor (GCD) of two
+    numbers. The algorithm is based on "the greatest common divisor of two numbers does not change
+    if the larger number is replaced by its difference with the smaller number"
+    @see https://en.wikipedia.org/wiki/Euclidean_algorithm
+
+    There are a number of different approaches to the algorithm, one of them being to:
+  1. Divide the larger number (a) by the smaller number (b)
+  2. If the remainder (a % b) = 0, then a is the GCD
+  3. If not, replace the value of a with the value of b, & replace the value of b with the value
+  of the remainder
+  4. Repeat until the remainder r= 0, at which point the C=GCD is a
+  */
+
+  private static int recursiveGCD(int larger, int smaller) {
+
+    // Here's the base case. The program will stop when the smaller number
+    // reaches 0;
+    if (smaller == 0) {
+      // When this happens, return the larger number.
+      return larger;
+    } else {
+      // Here is the recursive method. On each pass where the smaller number does not equal zero,
+      // the method calls itself again.
+      // The "smaller" value is now passed in to the method as the "larger" parameter and the
+      // remainder of larger / smaller is passed in directly as the result of larger mod smaller
+      //
+      return recursiveGCD(smaller, (larger % smaller));
+    }
+  }
+
+  private static int iterativeGCD(int larger, int smaller) {
+    // The iterative version uses a while loop to evaluate the value
+    // of "smaller". It requires a temporary variable be used in order that
+    // the larger variable may be swapped with the smaller, and the smaller
+    // variable with the remainder.
+    while (smaller != 0) {
+      int temp = smaller;
+      smaller = larger % smaller;
+      larger = temp;
+    }
+    // Once the smaller number reaches zero, return the larger
+    return larger;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("49 & 21");
+    System.out.println("Recursive: " + recursiveGCD(49, 21));
+    System.out.println("Iterative: " + iterativeGCD(49, 21));
+  }
+}
