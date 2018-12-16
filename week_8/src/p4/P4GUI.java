9b5a3e767d454b72f33aecdcd18dc830d8b84f8f
Jimmy Smutek <jimmy@smutek.net>
2018-12-16 02:57:05 -0500

    Moving along

--- week_8/src/p4/P4GUI.java
+++ week_8/src/p4/P4GUI.java
@@ -6,18 +6,69 @@ package p4;
  * Purpose: Defines the GUI and handles database interactions.
  */
 
+import java.util.Arrays;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.ActionListener;
 
+/*
+ * @todo: delete class instructions
+ *
+ * This programming project involves writing a program to manage a student database. The interface
+ * to the program should be a Swing based GUI that looks similar to the following:
+ *
+ * ((
+ * Id: [ text input field ]
+ * Name: [ text input field ]
+ * Major: [ text input field ]
+ * Choose Selection: [ Combo box ]
+ *
+ * A combo box should allow the user to select one of the four database actions shown.
+ *
+ * The database should be implemented as a HashMap, with the Id field as the key and a Student
+ * record as the value.
+ *
+ * The selected operation should be performed when the user clicks the Process Request button.
+ *
+ * If the user attempts to insert a key that is already in the database an error message should be
+ * displayed using a JOptionPane message dialog box.
+ *
+ * If the user attempts to delete, find or update a record that is not in the database, a message
+ * should also be displayed in a JOptionPane.
+ *
+ * After each successful operation is completed a JOptionPane window should be displayed confirming
+ * the success.
+ *
+ * In the case of a successful Find request, a window should pop up containing the student's Id,
+ * name, major and current GPA.
+ *
+ * When the user selects the Update request, the following JOptionPane windows should be displayed
+ * to gather information about a course that has just been completed.
+ *
+ * Choose Grade:
+ * [ combo box ]
+ *
+ * Choose credits:
+ * [ Combo box ]
+ *
+ * As a result, the Student record in the database will be updated accordingly. User input values
+ * should be checked and a warning message should be displayed in a JOptionPane for inappropriate
+ * values.
+ *
+ * When the main application window is closed, the student records from the database will be
+ * written into the text file outData.txt, each student data (including his/her GPA) on a separate
+ * line. The last line of the file will contain the total number of student records in the database
+ * and their average GPA value.
+ */
 public class P4GUI extends JFrame implements ActionListener {
 
-  // ************ Strings, for labels
-  private static final String[] LABELS = {"ID:", "Name:", "Major:", "Choose Selection:"};
+  // ************ Strings
   private static final String[] OPTIONS = {"Insert", "Delete", "Find", "Update"};
 
+  // ************ Labels
+  private JLabel id, name, major, selection;
   // ************ Fields
   private JTextField idField;
   private JTextField nameField;
@@ -51,12 +102,18 @@ public class P4GUI extends JFrame implements ActionListener {
     c.ipadx = 10;
 
     // ************ Create and add labels
-    for (int i = 0; i <= 3; i++) {
-      JLabel label = new JLabel(LABELS[i]);
-      add(label, c);
-      // Increment grid Y position
-      c.gridy++;
-    }
+    id = new JLabel("ID: ");
+    add(id, c);
+    c.gridy++;
+    name = new JLabel("Name: ");
+    add(name, c);
+    c.gridy++;
+    major = new JLabel("Major: ");
+    add(major, c);
+    c.gridy++;
+    selection = new JLabel("Selection: ");
+    add(selection, c);
+    c.gridy++;
 
     // ************ Create and add the submit button
     JButton submitButton = new JButton("Process Request");
@@ -68,20 +125,26 @@ public class P4GUI extends JFrame implements ActionListener {
     c.gridx = 1;
 
     // ************ Create and add the text fields
-    for (JTextField field : fields) {
-      field = new JTextField();
-      field.setColumns(FIELD_W);
-      add(field, c);
-      c.gridy ++;
-    }
+    idField = new JTextField();
+    idField.setColumns(FIELD_W);
+    add(idField, c);
+    c.gridy++;
+    nameField = new JTextField();
+    nameField.setColumns(FIELD_W);
+    add(nameField, c);
+    c.gridy++;
+    majorField = new JTextField();
+    majorField.setColumns(FIELD_W);
+    add(majorField, c);
+    c.gridy++;
 
     // ************ Create and add the combo box
     JComboBox comboBox = new JComboBox(OPTIONS);
-    add(comboBox);
+    comboBox.setSelectedIndex(0);
+    comboBox.addActionListener(this);
+    add(comboBox, c);
     c.gridy++;
-
   }
-
   /** Create and show the application GUI */
   private static void createAndShowApplicationGUI() {
     // Create and set up the main frame
@@ -95,7 +158,22 @@ public class P4GUI extends JFrame implements ActionListener {
 
   /** @param event ActionEvent */
   public void actionPerformed(ActionEvent event) {
-    // Event listener
+    // Test click @todo Remove
+    System.out.println("Click");
+    // So, when someone clicks, get all the values
+    // and kick them to the console @todo Remove
+    System.out.println(idField.getText());
+    System.out.println(nameField.getText());
+    System.out.println(majorField.getText());
+
+    if(comboBox.getSelectedItem() != null) {
+      String cIn = (String)comboBox.getSelectedItem();
+      System.out.println(cIn);
+
+    } else {
+      System.out.println("Still fucking null, bitches");
+    }
+
   }
 
   /**
